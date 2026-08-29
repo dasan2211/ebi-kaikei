c98a17b46132ccb1f6be3d801fb4becc5357cc4a
dasan2211 <ihiroto@ebihome.net>
2026-08-29 14:21:22 +0900

    feat(settings): restore appearance preferences on startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import { applyFontFamily, getStoredFontFamily } from "./lib/font-family";
+import { applyThemePreference, getStoredThemePreference } from "./lib/theme";
 import "./styles.css";
+
+applyFontFamily(getStoredFontFamily());
+applyThemePreference(getStoredThemePreference());
 
 const root = document.getElementById("root");
 
